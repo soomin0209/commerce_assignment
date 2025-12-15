a2e9c68be9f19a36b29f52f3e5c9272d9ae732f2
soomin0209 <soomin02094@gmail.com>
2025-12-16 00:28:04 +0900

    feat: 재고 0일 때 경고 메시지 출력, 장바구니가 비어있지 않으면 주문 관리 옵션 출력 (도전 lv 1)

--- src/CommerceSystem.java
+++ src/CommerceSystem.java
@@ -25,6 +25,11 @@ public class CommerceSystem {
                 categoryIndex++;
             }
             System.out.println("0. 종료");
+            if(!customer.getCart().isEmpty()) {
+                System.out.println("\n[ 주문 관리 ]");
+                System.out.println((categoryIndex) + ". 장바구니 확인");
+                System.out.println((categoryIndex + 1) + ". 주문 취소");
+            }
             System.out.print("입력: ");
 
             try {
@@ -33,11 +38,27 @@ public class CommerceSystem {
                 if (categoryNum == 0) {
                     System.out.println("\n커머스 플랫폼을 종료합니다.");
                     return;
-                }
-                if (categoryNum < 1 || categoryNum > categories.size()) {
+                } else if (categoryNum == (categoryIndex)){
+                    if (customer.getCart().isEmpty()) {
+                        System.out.println("다시 입력해주세요");
+                        continue;
+                    } else {
+                        System.out.println("장바구니 확인 !!!!");
+                        continue;
+                    }
+                } else if (categoryNum == (categoryIndex + 1)) {
+                    if (customer.getCart().isEmpty()) {
+                        System.out.println("다시 입력해주세요");
+                        continue;
+                    } else {
+                        System.out.println("주문 취소 !!!!");
+                        continue;
+                    }
+                }else if (categoryNum < 1 || categoryNum > categories.size()) {
                     System.out.println("다시 입력해주세요.");
                     continue;
                 }
+
                 Category selectCategory = categories.get(categoryNum - 1);
                 List<Product> products = selectCategory.getProducts();
 
@@ -68,6 +89,10 @@ public class CommerceSystem {
                             int cartNum = sc.nextInt();
 
                             if (cartNum == 1) {
+                                if (products.get(productNum - 1).getStock() == 0) {
+                                    System.out.println("\n" + products.get(productNum - 1).getProductName() + "의 재고가 부족합니다.");
+                                    break;
+                                }
                                 customer.addToCart(products.get(productNum - 1));
                                 System.out.println("\n" + products.get(productNum - 1).getProductName() + "이(가) 장바구니에 추가되었습니다.");
                                 break;
