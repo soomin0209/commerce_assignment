33038fee25c305ca6e6171fe5b9cda56c34ebc4e
soomin0209 <soomin02094@gmail.com>
2025-12-15 14:32:02 +0900

    refactor: 상품 관리, 사용자 입력 처리를 CommerceSystem 클래스로 분리 (step 2)

--- src/CommerceSystem.java
+++ src/CommerceSystem.java
@@ -1,2 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class CommerceSystem {
+
+    private List<Product> products = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+
+    // 생성자
+    public CommerceSystem(List<Product> products) {
+        this.products = products;
+    }
+
+    // 기능
+    public void start() {
+        System.out.println("[ 실시간 커머스 플랫폼 = 전자제품 ]");
+        for (int i = 0; i < products.size(); i++) {
+            System.out.println((i + 1) + ". " + products.get(i));
+        }
+        System.out.println("0. 종료");
+
+        while (true) {
+            System.out.print("입력: ");
+
+            try {
+                int num = sc.nextInt();
+                switch (num) {
+                    case 1:
+                        System.out.println(products.get(0));
+                        break;
+                    case 2:
+                        System.out.println(products.get(1));
+                        break;
+                    case 3:
+                        System.out.println(products.get(2));
+                        break;
+                    case 4:
+                        System.out.println(products.get(3));
+                        break;
+                    case 0:
+                        System.out.println("커머스 플랫폼을 종료합니다.");
+                        return;
+                    default:
+                        System.out.println("다시 입력해주세요.");
+                }
+            } catch (Exception e) {
+                System.out.println("다시 입력해주세요.");
+                sc.nextLine();
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
         List<Product> products = new ArrayList<>();
+        CommerceSystem commerceSystem = new CommerceSystem(products);
 
         Product galaxy = new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 50);
         Product iphone = new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 50);
@@ -18,40 +18,6 @@ public class Main {
         products.add(macbook);
         products.add(airpods);
 
-        System.out.println("[ 실시간 커머스 플랫폼 = 전자제품 ]");
-        for (int i = 0; i < products.size(); i++) {
-            System.out.println((i + 1) + ". " + products.get(i));
-        }
-        System.out.println("0. 종료");
-
-        while (true) {
-            System.out.print("입력: ");
-
-            try {
-                int num = sc.nextInt();
-                switch (num) {
-                    case 1:
-                        System.out.println(products.get(0));
-                        break;
-                    case 2:
-                        System.out.println(products.get(1));
-                        break;
-                    case 3:
-                        System.out.println(products.get(2));
-                        break;
-                    case 4:
-                        System.out.println(products.get(3));
-                        break;
-                    case 0:
-                        System.out.println("커머스 플랫폼을 종료합니다.");
-                        return;
-                    default:
-                        System.out.println("다시 입력해주세요.");
-                }
-            } catch (Exception e) {
-                System.out.println("다시 입력해주세요.");
-                sc.nextLine();
-            }
-        }
+        commerceSystem.start();
     }
 }
