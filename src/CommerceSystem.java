205cccde17ac595c7710494060f6d45c09461c6f
soomin0209 <soomin02094@gmail.com>
2025-12-15 20:06:12 +0900

    refactor: 기존 switch문을 if문으로 변경해 확장성 개선

--- src/CommerceSystem.java
+++ src/CommerceSystem.java
@@ -27,61 +27,44 @@ public class CommerceSystem {
 
             try {
                 int categoryNum = sc.nextInt();
-                switch (categoryNum) {
-                    case 1:
-                        List<Product> electronics = categories.get(0).getProducts();
 
-                        System.out.println(categories.get(0).getCategoryName() + " 카테고리");
-                        int productIndex = 1;
-                        for (Product electronic : electronics) {
-                            System.out.println(productIndex + ". " + electronic);
-                            productIndex++;
+                if (categoryNum == 0) {
+                    System.out.println("커머스 플랫폼을 종료합니다.");
+                    return;
+                }
+                if (categoryNum < 1 || categoryNum > categories.size()) {
+                    System.out.println("다시 입력해주세요.");
+                    continue;
+                }
+                Category selectCategory = categories.get(categoryNum - 1);
+                List<Product> products = selectCategory.getProducts();
 
+                System.out.println(selectCategory.getCategoryName() + " 카테고리");
+                int productIndex = 1;
+                for (Product product : products) {
+                    System.out.println(productIndex + ". " + product);
+                    productIndex++;
+                }
+                System.out.println("0. 뒤로가기");
+
+                while (true) {
+                    System.out.print("입력: ");
+
+                    try {
+                        int productNum = sc.nextInt();
+
+                        if (productNum == 0) {
+                            break;
                         }
-                        System.out.println("0. 뒤로가기");
-
-                        while (true) {
-                            System.out.print("입력: ");
-
-                            try {
-                                int productNum = sc.nextInt();
-
-                                if (productNum == 0) {
-                                    break;
-                                }
-                                switch (productNum) {
-                                    case 1:
-                                        System.out.println(electronics.get(0));
-                                        break;
-                                    case 2:
-                                        System.out.println(electronics.get(1));
-                                        break;
-                                    case 3:
-                                        System.out.println(electronics.get(2));
-                                        break;
-                                    case 4:
-                                        System.out.println(electronics.get(3));
-                                        break;
-                                    default:
-                                        System.out.println("다시 입력해주세요.");
-                                }
-                            } catch (Exception e) {
-                                System.out.println("다시 입력해주세요.");
-                                sc.nextLine();
-                            }
+                        if (productNum < 1 || productNum > products.size()) {
+                            System.out.println("다시 입력해주세요.");
+                            continue;
                         }
-                        break;
-                    case 2:
-                        System.out.println(categories.get(1));
-                        break;
-                    case 3:
-                        System.out.println(categories.get(2));
-                        break;
-                    case 0:
-                        System.out.println("커머스 플랫폼을 종료합니다.");
-                        return;
-                    default:
+                        System.out.println(products.get(productNum - 1));
+                    } catch (Exception e) {
                         System.out.println("다시 입력해주세요.");
+                        sc.nextLine();
+                    }
                 }
             } catch (Exception e) {
                 System.out.println("다시 입력해주세요.");
